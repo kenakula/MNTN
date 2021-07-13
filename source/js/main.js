00bc0b95dee4270ffb348cd638fa9442c40d954c
AgaevKen <kenkosh@yandex.ru>
2021-07-13 08:08:36 +0300

    исправляет ошибки по валидатору

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,6 @@
 import { ieFix } from './utils/ie-fix';
 import { iosVhFix } from './utils/ios-vh-fix';
+import './utils/modernizr';
 
 import { initModals } from './modules/init-modals';
 import { menuToggler, onMenuTogglerToggleClass } from './modules/toggler';
